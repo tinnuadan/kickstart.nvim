5403d334a5ae0da92a541c6f733feedb39a8783d
Veigel, Jochen.ext <jochen.veigel.ext@karlstorz.com>
2025-03-28 09:53:21 +0100

    lualine

--- lua/custom/plugins/ui/lualine.lua
+++ lua/custom/plugins/ui/lualine.lua
@@ -1,49 +1,54 @@
 return {
-	{
-		'nvim-lualine/lualine.nvim',
-		dependencies = { 'nvim-tree/nvim-web-devicons' },
-		config = function()
-			require('lualine').setup {
-				options = {
-					icons_enabled = true,
-					theme = 'auto',
-					component_separators = { left = '', right = '' },
-					section_separators = { left = '', right = '' },
-					disabled_filetypes = {
-						statusline = {},
-						winbar = {},
-					},
-					ignore_focus = {},
-					always_divide_middle = true,
-					always_show_tabline = true,
-					globalstatus = false,
-					refresh = {
-						statusline = 100,
-						tabline = 100,
-						winbar = 100,
-					}
-				},
-				sections = {
-					lualine_a = { 'mode' },
-					lualine_b = { 'branch', 'diff', 'diagnostics' },
-					lualine_c = { 'filename' },
-					lualine_x = { 'encoding', 'fileformat', 'filetype' },
-					lualine_y = { 'progress' },
-					lualine_z = { 'location' }
-				},
-				inactive_sections = {
-					lualine_a = {},
-					lualine_b = {},
-					lualine_c = { 'filename' },
-					lualine_x = { 'location' },
-					lualine_y = {},
-					lualine_z = {}
-				},
-				tabline = {},
-				winbar = {},
-				inactive_winbar = {},
-				extensions = {}
-			}
-		end
-	}
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      local git_blame = require 'gitblame'
+      -- This disables showing of the blame text next to the cursor
+      vim.g.gitblame_display_virtual_text = 0
+
+      require('lualine').setup {
+        options = {
+          icons_enabled = true,
+          theme = 'auto',
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
+          disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+          },
+          ignore_focus = {},
+          always_divide_middle = true,
+          always_show_tabline = true,
+          globalstatus = false,
+          refresh = {
+            statusline = 100,
+            tabline = 100,
+            winbar = 100,
+          },
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filename' },
+          lualine_x = { { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available } },
+          lualine_y = { 'encoding', 'fileformat', 'filetype' },
+          -- lualine_y = { 'progress' },
+          lualine_z = { 'location' },
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { 'filename' },
+          lualine_x = { 'location' },
+          lualine_y = {},
+          lualine_z = {},
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {},
+      }
+    end,
+  },
 }
