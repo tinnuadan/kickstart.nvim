ca8ee018c7f92f2c1c20baa27c5aa0d3e544dff0
Veigel, Jochen.ext <jochen.veigel.ext@karlstorz.com>
2025-03-28 09:53:05 +0100

    tab bar

--- lua/custom/plugins/ui/barbar.lua
+++ lua/custom/plugins/ui/barbar.lua
@@ -13,6 +13,11 @@ return {
       -- animation = true,
       -- insert_at_start = true,
       -- …etc.
+      clickable = false,
+      icons = {
+        button = '',
+      },
+      exclude_name = { 'bash' },
     },
     version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
